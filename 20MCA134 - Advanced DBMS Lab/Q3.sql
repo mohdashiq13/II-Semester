92527ffa495f1d3269bbd77a395b7c61b3a5aadf
mohdashiq13 <muhamedashiq2024b@mca.ajce.in>
2023-06-24 11:44:25 +0530

    CO1

--- 20MCA134 - Advanced DBMS Lab/Q3.sql
+++ 20MCA134 - Advanced DBMS Lab/Q3.sql
@@ -2,3 +2,4 @@ USE bank;
 SELECT DISTINCT deposit.cname FROM deposit LEFT JOIN borrow ON deposit.cname != borrow.cname ORDER BY deposit.cname;
 SELECT deposit.cname FROM deposit JOIN borrow ON deposit.cname = borrow.cname;
 SELECT cname FROM deposit WHERE bname IN (SELECT bname FROM deposit WHERE cname LIKE "Sunil");
+SELECT customer.cname FROM customer INNER JOIN deposit ON deposit.cname = ;
