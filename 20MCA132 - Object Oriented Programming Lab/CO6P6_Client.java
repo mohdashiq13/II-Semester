0662554c4391e9e43fc63b547c65ff913db04957
mohdashiq13 <muhamedashiq2024b@mca.ajce.in>
2023-07-11 10:48:15 +0530

    CO6

---
+++ 20MCA132 - Object Oriented Programming Lab/CO6P6_Client.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class CO6P6_Client{
+	public static void main(String []args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		DatagramSocket dataSocket = new DatagramSocket();
+		InetAddress ip = InetAddress.getLocalHost();
+		System.out.println("Host Name with IP Address :"+ip);
+		byte buff[] = null;
+		while(true){
+			String chat = sc.next();
+			buff = chat.getBytes(); //Converting inputed chat to Bytes
+			System.out.println(buff);
+										  //DatagramPacket(Bytes, Byte Length, Host IP, Port Num); Setting up the Packet to be Send
+			DatagramPacket packetSend = new DatagramPacket(buff, buff.length, ip, 530);
+			dataSocket.send(packetSend); // Sending the Packet
+			
+			if(chat.equals("bye"))
+				break;
+		}
+	}
+}
---
+++ 20MCA132 - Object Oriented Programming Lab/CO6P6_Server.java
@@ -0,0 +1,5 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.SocketException;
