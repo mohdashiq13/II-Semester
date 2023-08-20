3ea12059526dad9198e38e05b8e0d3bb88e0feda
mohdashiq13 <muhamedashiq2024b@mca.ajce.in>
2023-08-20 10:58:05 +0530

    CO4

--- 20MCA132 - Object Oriented Programming Lab/CO4P7.java
+++ 20MCA132 - Object Oriented Programming Lab/CO4P7.java
@@ -1,2 +1,31 @@
+// Producer/Consumer using ITC
+
+import java.util.concurrent.BlockingQueue;
+
+class Producer implements Runnable{
+    BlockingQueue<Integer> product;
+    public Producer(BlockingQueue<Integer> obj){
+        product = obj;
+    }
+    @Override
+    public void run() {
+    }
+}
+
+class Consumer implements Runnable{
+    @Override
+    public void run() {
+    }
+}
 public class CO4P7 {
+    public static void main(String[] args) {
+        Producer p = new Producer(10);
+        Consumer c = new Consumer();
+
+        Thread pt = new Thread(p);
+        Thread ct = new Thread(c);
+
+        pt.start();
+        ct.start();
+    }
 }
